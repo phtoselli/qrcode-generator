0d56b6b0cd513edfa63aca4b3d51c6e7d087b6ad
phtoselli <phtoselli@gmail.com>
2023-10-20 18:49:53 -0300

    Fix: change input properties

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,14 +12,17 @@ export default function Home() {
   const [scale, changeScale] = useState(4);
   const [size, changeSize] = useState(600);
   const [margin, changeMargin] = useState(4);
+  const [version, changeVersion] = useState(2);
   const [name, changeName] = useState('qrcode');
   const [qrcodeLink, setQrcodeLink] = useState('');
+  const [format, changeFormat] = useState('image/png');
 
   function generateLink(url) {
     const config = {
       scale: Number(scale),
       windth: Number(size),
       margin: Number(margin),
+      version: Number(version),
     };
 
     QRCodeLink.toDataURL(url, config, (err, url) => {
@@ -30,7 +33,7 @@ export default function Home() {
 
   useEffect(() => {
     generateLink(link);
-  }, [size, link, name, margin, scale]);
+  }, [size, link, name, margin, scale, version, format]);
 
 
 
@@ -49,33 +52,54 @@ export default function Home() {
         <div className='row container justify-content-center p-2'>
           <div className="col-12 col-md-3 text-secondary">
             <label htmlFor="size">Size(px):</label>
-            <input  className="form-control" type="number" id='size' value={size} onChange={(e) => changeSize(e.target.value)}/>
+            <input  className="form-control" type="number" id='size' value={size} onChange={(e) => changeSize(e.target.value)} autoComplete="off"/>
           </div>
 
           <div className="col-12 col-md-3 text-secondary">
             <label htmlFor="margin">Margin(px):</label>
-            <input  className="form-control" type="number" id='margin' value={margin} onChange={(e) => changeMargin(e.target.value)}/>
+            <input  className="form-control" type="number" id='margin' value={margin} onChange={(e) => changeMargin(e.target.value)} autoComplete="off"/>
           </div>
         </div>
 
         <div className='row container justify-content-center p-2'>
           <div className="col-12 col-md-6 text-secondary">
             <label htmlFor="scale">Scale:</label>
-            <input  className="form-control" type="number" id='scale' value={scale} onChange={(e) => changeScale(e.target.value)}/>
+            <input  className="form-control" type="number" id='scale' value={scale} onChange={(e) => changeScale(e.target.value)} autoComplete="off"/>
+          </div>
+        </div>
+
+        <div className='row container justify-content-center p-2'>
+          <div className="col-12 col-md-3 text-secondary">
+              <label htmlFor="version">Version:</label>
+              <select className="form-select" aria-label="version" id='version' value={version} onChange={(e) => changeVersion(e.target.value)} autoComplete="off">
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+              </select>
+            </div>
+
+          <div className="col-12 col-md-3 text-secondary">
+            <label htmlFor="format">Format:</label>
+            <select className="form-select" aria-label="format" id="format" value={format} onChange={(e) => changeFormat(e.target.value)}>
+              <option value="image/png">.png</option>
+              <option value="image/jpeg">.jpeg</option>
+              <option value="image/webp">.webp</option>
+            </select>
           </div>
         </div>
 
         <div className='row container justify-content-center p-2'>
           <div className="col-12 col-md-6 text-secondary">
             <label htmlFor="name">File name:</label>
-            <input className="form-control" type="text" id='name' value={name} onChange={(e) => changeName(e.target.value)}/>
+            <input className="form-control" type="text" id='name' value={name} onChange={(e) => changeName(e.target.value)} autoComplete="off"/>
           </div>
         </div>
 
         <div className='row container justify-content-center p-2'>
           <div className="col-12 col-md-6 text-secondary">
-            <label htmlFor="margin">QRCode link:</label>
-            <input className="form-control" type="text" id='margin' value={link} onChange={(e) => changeLink(e.target.value)}/>
+            <label htmlFor="link">QRCode link:</label>
+            <input className="form-control" type="text" id='link' value={link} onChange={(e) => changeLink(e.target.value)} autoComplete="off"/>
           </div>
         </div>
 
