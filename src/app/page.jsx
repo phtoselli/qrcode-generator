6e1f2a6a9b9ab70c8c69329a7c1cec42fbeb5fb1
phtoselli <phtoselli@gmail.com>
2023-10-20 18:04:25 -0300

    Add: create a input to change values

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,62 +2,82 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import QRCode from 'react-qr-code';
 import QRCodeLink from 'qrcode';
 
 export default function Home() {
+  const [size, changeSize] = useState(800);
   const [link, changeLink] = useState('');
+  const [name, changeName] = useState('qrcode');
+  const [margin, changeMargin] = useState(2);
   const [qrcodeLink, setQrcodeLink] = useState('');
 
-  function generateLink(url) {
+  function generateLink() {
     const config = {
-      windth: 800,
-      margin: 3
+      windth: size,
+      margin: margin
     }
 
-    QRCodeLink.toDataURL(url, config, (err, url) => {
+    QRCodeLink.toDataURL(link, config, (err, url) => {
       if(err) console.error(err);
       setQrcodeLink(url)
     });
   }
 
-  function generateQRCode(event) {
-    changeLink(event.target.value);
-    generateLink(event.target.value);
-  }
+  useEffect(() => {
+    generateLink()
+    console.log(size);
+  }, [size, link, name, margin]);
+
+
 
   return (
-    <main>
-        <div className="d-flex flex-column justify-content-center align-items-center" style={{ width: '100vw', height: '100vh' }}>
-          <div className="container d-flex flex-column justify-content-center align-items-center">
+    <main className='container-fluid'>
+      <div className='container d-flex flex-column align-items-center justify-content-center'>
 
-            <div id="qrcode">
-              <QRCode value={link}/>
-            </div>
+        <hr className='mt-5' style={{ opacity: '0%' }}/>
 
-            <hr className='mt-4 mb-4' style={{ opacity: '0%' }}/>
+        <div>
+          <QRCode value={link}/>
+        </div>
 
-            <div className='container' id="form">
-              <form className='form container d-flex flex-column justify-content-center align-items-center'>
-                <label className="form-label" htmlFor="input"></label>
-                <input
-                  className="form-control"
-                  id="input"
-                  type="text"
-                  placeholder='Digit your link here ...'
-                  value={link}
-                  onChange={(e) => generateQRCode(e)}
-                  style={{ maxWidth: '400px' }}
-                />
-                <hr className='mt-3 mb-2' style={{ opacity: '0%' }}/>
-                <a className='btn btn-primary container-fluid' style={{ maxWidth: '400px' }} href={qrcodeLink} download={`qrcode.png`}>Download QRCode</a>
-              </form>
-            </div>
+        <hr className='mt-5 mb-5' />
 
+        <div className='row container justify-content-center p-2'>
+          <div className="col-12 col-md-3 text-secondary">
+            <label htmlFor="size">Size(px):</label>
+            <input  className="form-control" type="number" id='size' value={size} onChange={(e) => changeSize(e.target.value)}/>
+          </div>
+
+          <div className="col-12 col-md-3 text-secondary">
+            <label htmlFor="margin">Margin(px):</label>
+            <input  className="form-control" type="number" id='margin' value={margin} onChange={(e) => changeMargin(e.target.value)}/>
           </div>
         </div>
+
+        <div className='row container justify-content-center p-2'>
+          <div className="col-12 col-md-6 text-secondary">
+            <label htmlFor="name">File name:</label>
+            <input className="form-control" type="text" id='name' value={name} onChange={(e) => changeName(e.target.value)}/>
+          </div>
+        </div>
+
+        <div className='row container justify-content-center p-2'>
+          <div className="col-12 col-md-6 text-secondary">
+            <label htmlFor="margin">QRCode link:</label>
+            <input className="form-control" type="text" id='margin' value={link} onChange={(e) => changeLink(e.target.value)}/>
+          </div>
+        </div>
+
+        <div className='row container justify-content-center p-2'>
+          <div className="col-12 col-md-6 text-secondary">
+            <a className='btn btn-primary container-fluid' href={qrcodeLink} download={`${name}.png`}>Download</a>
+          </div>
+        </div>
+
+      </div>
     </main>
   )
 }
